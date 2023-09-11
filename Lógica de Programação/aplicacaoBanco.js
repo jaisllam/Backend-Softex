6b8e44d4f8df5542bfaec85bb4865da731666540
Jaíslla Macedo <jaislla@outlook.com>
2023-09-11 16:09:03 -0300

    featuring: consultas básicas de um app

--- Lógica de Programação/aplicacaoBanco.js
+++ Lógica de Programação/aplicacaoBanco.js
@@ -1,66 +1,61 @@
 const readline = require('readline-sync');
 
-class Banco{
-  constructor(conta,saldo,tipo, agencia){
+class Banco {
+  constructor(conta, saldo, tipo, agencia) {
     this.conta = conta;
     this.saldo = saldo;
     this.tipo = tipo;
     this.agencia = agencia;
   }
 
-  
-  function mostrarSaldo(){
+  mostrarSaldo() {
     console.log(this.saldo.toFixed(2));
   }
-  
-  
-  function fazerDeposito(quantiadeposito){
-    let depositado = this.saldo+quantiadeposito;
+
+  fazerDeposito(quantiadeposito) {
+    let depositado = this.saldo + quantiadeposito;
     console.log(depositado.toFixed(2));
   }
-  
-  
-  function fazerSaque(quantiasaque){
-    if(quantiasaque>this.saldo){
-      console.log("Operação não realizada. Entre com quantia adequada");
-    }else{
+
+  fazerSaque(quantiasaque) {
+    if (quantiasaque > this.saldo) {
+      console.log("Operação não realizada. Entre com uma quantia adequada");
+    } else {
       let saque = this.saldo - quantiasaque;
       console.log("Aguarde a saída do dinheiro");
       console.log(`Seu saldo atual é ${saque.toFixed(2)}`);
     }
   }
-  
-  
-  function mostrarConta(){
-    console.log(`O número da sua  conta é ${this.conta}`);
+
+  mostrarConta() {
+    console.log(`O número da sua conta é ${this.conta}`);
   }
-  
 }
 
-let cliente = new Banco(32788, 6300.50,"Corrente",34552);
+let cliente = new Banco(32788, 6300.50, "Corrente", 34552);
 
 console.log("Bem vindo!");
-console.log("1-Mostrar saldo");
-console.log("2-Fazer depósito");
-console.log("3-Fazer saque")
-console.log("4-Mostrar conta")
-let opcao = readline.question("Qual das opçoes acima deseja realiza?");
+console.log("1 - Mostrar saldo");
+console.log("2 - Fazer depósito");
+console.log("3 - Fazer saque");
+console.log("4 - Mostrar conta");
+let opcao = readline.question("Qual das opções acima deseja realizar?");
 
-switch(opcao){
-    case'1':
-      cliente.mostrarSaldo();
-      break;
-    case'2':
-      let quantiadeposito = readline.parseFloat("Digite qual valor deseja depositar: ");
-      cliente.fazerDeposito(quantiadeposito);
-      break;
-    case'3':
-      let quantiasaque = readline.parseFloat("Quanto deseja sacar:");
-      cliente.fazerSaque(quantiasaque);
-      break;
-    case'4':
-      cliente.mostrarConta();
-      break;
-    default:
-    console.log("Numero invalido");
+switch (opcao) {
+  case '1':
+    cliente.mostrarSaldo();
+    break;
+  case '2':
+    let quantiadeposito = parseFloat(readline.question("Digite qual valor deseja depositar: "));
+    cliente.fazerDeposito(quantiadeposito);
+    break;
+  case '3':
+    let quantiasaque = parseFloat(readline.question("Quanto deseja sacar:"));
+    cliente.fazerSaque(quantiasaque);
+    break;
+  case '4':
+    cliente.mostrarConta();
+    break;
+  default:
+    console.log("Número inválido");
 }
